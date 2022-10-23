958011a1d2e33cff932e87caf1f5afeb9e9786a5
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-23 11:39:25 +0300

    Completed assignment

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
 ## code your solution here. 
+class Cat
+    def meow
+        puts "meow!"
+    end
+# Cat receives attr_accessor
+    attr_accessor :name
+    # Cat has a name
+    def name
+        @name
+    end
+
+
+    
+
+end
